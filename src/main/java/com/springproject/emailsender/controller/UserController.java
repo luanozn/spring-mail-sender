974771c5224bbcb27f3881ad5d7dea231bff812f
Luan de Castro Ribeiro <luan.ribeiro@estudante.ifgoiano.edu.br>
2022-06-03 22:43:18 -0300

    Resolução de bugs e implementação do email

--- src/main/java/com/springproject/emailsender/controller/UserController.java
+++ src/main/java/com/springproject/emailsender/controller/UserController.java
@@ -16,7 +16,6 @@ public class UserController {
 
     @Autowired
     UserServiceImpl userService;
-
     @Autowired
     EmailServiceImpl emailService;
 
@@ -33,7 +32,30 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User user){
         userService.insert(user);
-        emailService.sendEmail("luanorizona1@gmail.com", "Teste", "Este é um email teste");
+        emailService.sendEmail(
+                user.getEmail(),
+                "Confirmação de Cadastro",
+                String.format("<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<head>\n" +
+                        "<!-- HTML Codes by Quackit.com -->\n" +
+                        "<title>\n" +
+                        "</title>\n" +
+                        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                        "<style>\n" +
+                        "body {background-repeat:no-repeat;background-position:center center;background-attachment:fixed;}\n" +
+                        "h2{font-family:Impact, sans-serif;font-variant:small-caps;color:#58181f;padding-bottom:20px}\n" +
+                        "p {text-align:center;font-family:Arial, sans-serif;font-size:15px;font-style:italic;font-weight:bold;color:#000000}" +
+                        "att {padding-top:20px}\n" +
+                        "</style>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<h2>Cadastro realizado com sucesso!</h2>\n" +
+                        "<p>Parabéns %s, seu usuário foi cadastrado com sucesso.</p>\n" +
+                        "<p id='att'>Atenciosamente</p>\n" +
+                        "<p>Projeto Mail Sender</p>\n" +
+                        "</body>\n" +
+                        "</html>\n", user.getName()));
         return ResponseEntity.ok(user);
     }
 
--- src/main/java/com/springproject/emailsender/service/impl/EmailServiceImpl.java
+++ src/main/java/com/springproject/emailsender/service/impl/EmailServiceImpl.java
@@ -4,7 +4,12 @@ import com.springproject.emailsender.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.Objects;
 
 @Service("emailService")
 public class EmailServiceImpl implements EmailService {
@@ -16,13 +21,19 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmail(String destiny, String title, String content) {
-        SimpleMailMessage message = new SimpleMailMessage(preConfiguredMessage);
-        message.setTo(destiny);
-        message.setSubject(title);
-        message.setText(content);
-        System.out.println(javaMailSender.getUsername());
-        System.out.println(javaMailSender.getPassword());
-        javaMailSender.send(message);
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        try {
+            helper.setTo(destiny);
+            helper.setFrom(Objects.requireNonNull(javaMailSender.getUsername()));
+            helper.setSubject(title);
+            helper.setText(content, true);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+        javaMailSender.send(mimeMessage);
+
     }
 
 
