999304146e64cfef80a7449580c7ba824ee40e39
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2023-06-03 19:43:22 -0300

    Mongodb refactored

--- src/main/java/com/springproject/emailsender/model/exceptions/UserNotFoundException.java
+++ src/main/java/com/springproject/emailsender/model/exceptions/UserNotFoundException.java
@@ -1,8 +1,6 @@
 package com.springproject.emailsender.model.exceptions;
 
-import javax.persistence.NoResultException;
-
-public class UserNotFoundException extends NoResultException {
+public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String username){
         super(username + " not found.");
